2ec0064c3daf14b443bc92e425345c5aff559b43
andronixmd <andronicsergei97@gmail.com>
2019-05-09 22:55:03 +0300

    list

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,9 +1,19 @@
 from app import app
 from flask import render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
+from sqlalchemy import or_
+from app.models.user import User
 
 
 @app.route('/')
 @login_required
 def index():
-    return render_template('pages/index.html')
+    users = User.query.filter(
+        User.id != current_user.id,
+        or_(
+            User.admin_id == current_user.id,
+            User.teacher_id == current_user.id,
+            User.student_id == current_user.id
+        )
+    ).all()
+    return render_template('pages/index.html', users=users)
